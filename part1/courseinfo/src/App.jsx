719539fa9e0424f45fbb4474ca9fb97bd4f38a99
Juan Rojas <juanrojas043@gmail.com>
2024-07-19 15:33:57 -0500

    1.2: Información del Curso, paso 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
   );
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
@@ -19,6 +19,16 @@ const Content = (props) => {
         {props.part3} {props.exercise3}
       </p>
     </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part1={props.part1} exercise1={props.exercise1} />
+      <Part part2={props.part2} exercise2={props.exercise2} />
+      <Part part3={props.part3} exercise3={props.exercise3} />
+    </div>
   );
 };
 
