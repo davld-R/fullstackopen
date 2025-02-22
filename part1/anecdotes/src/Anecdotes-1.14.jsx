6a7cde18e45847ab533818162d212b15ec5479b3
Juan David Rojas <juanrojas043@gmail.com>
2025-02-21 19:00:29 -0500

    anecdotes, paso 3

---
+++ part1/anecdotes/src/Anecdotes-1.14.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+
+const Text = ({ text }) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
+};
+
+const Anecdote14 = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [vote, setVote] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+  });
+  const [maxNumber, setMaxNumber] = useState(0);
+  const [maxAnecdote, setMaxAnecdote] = useState(0);
+
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+    console.log(selected);
+  };
+
+  const handleClickVote = () => {
+    setVote({ ...vote, [selected]: vote[selected] + 1 });
+  };
+
+  for (let index = 0; index < Object.keys(vote).length; index++) {
+    const element = vote[index];
+    if (element > maxNumber) {
+      setMaxNumber(element);
+      setMaxAnecdote(index);
+    }
+  }
+
+  return (
+    <div>
+      <Text text="Anecdote of the day" />
+      <div>{anecdotes[selected]}</div>
+      <div>has {vote[selected]} votes</div>
+      <button onClick={handleClickVote}>vote</button>
+      <button onClick={handleClick}>next anecdote</button>
+      <Text text={'Anecdote with most votes'} />
+      <div>{anecdotes[maxAnecdote]}</div>
+      <div>Has {maxNumber} votes</div>
+    </div>
+  );
+};
+
+export default Anecdote14;
--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,11 +1,13 @@
 import Anecdote12 from './Anecdotes-1.12';
 import Anecdote13 from './Anecdotes-1.13';
+import Anecdote14 from './Anecdotes-1.14';
 
 const App = () => {
   return (
     <>
       {/* <Anecdote12 /> */}
-      <Anecdote13 />
+      {/* <Anecdote13 /> */}
+      <Anecdote14 />
     </>
   );
 };
