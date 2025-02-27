bb2c39381d19de326b4960a4c5ac4389b28c2b5d
Juan David Rojas <juanrojas043@gmail.com>
2025-02-27 16:14:08 -0500

    hight order - comunication

---
+++ part2/Higher-order-functions.js
@@ -0,0 +1,50 @@
+// function triple(x) {
+//   return x * 3;
+// }
+
+// let triple = function (x) {
+//   return x * 3;
+// };
+
+// const triple = (x) => {
+//   return x * 3;
+// };
+
+// // Filter // //
+let animals = [
+  { name: 'Fluffykins', species: 'rabbit' },
+  { name: 'Caro', species: 'dog' },
+  { name: 'Hamilton', species: 'dog' },
+  { name: 'Harold', species: 'fish' },
+  { name: 'Ursula', species: 'cat' },
+  { name: 'Jimmy', species: 'fish' },
+];
+
+// let dogs = animals.filter(function (animal) {
+//   return animal.species === 'dog';
+// });
+
+// let isCat = animals.filter((element) => element.species === 'cat');
+
+// // Map // //
+
+// let names = animals.map((animal) => {
+//   return animal.name;
+// });
+
+// let names2 = animals.map((e) => e.name);
+
+// console.log(names2);
+
+// // reduce() // //
+
+let orders = [
+  { amount: 250 },
+  { amount: 400 },
+  { amount: 100 },
+  { amount: 325 },
+];
+
+// let totalAmount = orders.reduce((sum, order) => sum + order.amount, 0);
+
+// console.log(totalAmount);
